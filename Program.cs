e133aa45886055b23c16b75acee0e7a78b147fe6
FrankA94 <96749300+FrankA94@users.noreply.github.com>
2022-05-05 13:54:52 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Returverdier
+{
+    internal class Program
+    {
+
+       static int tall1 = 3;
+       static int tall2 = 4;
+       static string ingenTing = "Denne metoden returnerer ingenting";
+
+       static void Main(string[] args)
+        {
+            var toTall = TallSum(tall1, tall2);
+            Console.WriteLine(toTall);
+            var ikkeNoe = Nada(ingenTing);
+            Console.WriteLine(ikkeNoe);
+            
+         
+        }
+        static int TallSum(int tall1, int tall2)
+        {
+            return tall1 + tall2;
+        }
+        static string Nada(string ingenTing)
+        {
+            return ingenTing;
+        }
+       
+
+    }
+}
